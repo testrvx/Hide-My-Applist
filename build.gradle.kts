8259e32c24189701c7e2104f30c155367d23ff68
DanGLES3 <switchtegra@gmail.com>
2024-08-07 02:21:57 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ fun Project.configureBaseExtension() {
                 signingConfig = config ?: signingConfigs["debug"]
             }
             named("release") {
-                isMinifyEnabled = true
+                isMinifyEnabled = false
                 proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             }
         }
